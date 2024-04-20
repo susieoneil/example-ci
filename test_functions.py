635c01ddc8d0e41acfb483aa7a728615cdee6866
Susie ONeil <susie@oneil.com.au>
2024-04-20 12:19:02 +0800

    restore function subtract; fixes #1

--- test_functions.py
+++ test_functions.py
@@ -6,7 +6,6 @@ def test_add():
     assert add(2, 3) == 5
     assert add('space', 'ship') == 'spaceship'
 
-# uncomment the following test in step 5
 def test_subtract():
     assert subtract(2, 3) == -1
 
